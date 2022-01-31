0c694edbc0a8decc715e3df33abb0003583c5ab0
Vladimir Sirenko <81309430+V-Sirenko@users.noreply.github.com>
2022-01-31 21:37:17 +0200

    Added error midlleware.

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,9 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', router);
 
+app.use(errorHandler);
+
 app.get('/', (req,res) => {
     res.status(200).json({message: 'WORKING!!'})
 });
+
+
 
 const start = async () => {
     try {
