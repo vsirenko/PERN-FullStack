0028ba892112704bf7b75baaf00f98ea68bcc025
Vladimir Sirenko <81309430+V-Sirenko@users.noreply.github.com>
2022-02-01 10:08:46 +0200

    Finished device controller logic. Added express static.

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 const fileUpload = require('express-fileupload');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload({}));
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use('/api', router);
 
 app.use(errorHandler);
--- server/models/models.js
+++ server/models/models.js
@@ -1,20 +1,20 @@
-const sequelize = require('../db');
-const { DataTypes } = require('sequelize');
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
 
-const User = sequelize.define('user',{
+const User = sequelize.define('user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    email: {type: DataTypes.STRING, unique: true},
+    email: {type: DataTypes.STRING, unique: true,},
     password: {type: DataTypes.STRING},
-    role: {type: DataTypes.STRING, defaultValue: 'USER'},
-});
+    role: {type: DataTypes.STRING, defaultValue: "USER"},
+})
 
 const Basket = sequelize.define('basket', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-});
+})
 
 const BasketDevice = sequelize.define('basket_device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-});
+})
 
 const Device = sequelize.define('device', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
@@ -27,61 +27,64 @@ const Device = sequelize.define('device', {
 const Type = sequelize.define('type', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
-});
+})
 
 const Brand = sequelize.define('brand', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     name: {type: DataTypes.STRING, unique: true, allowNull: false},
-});
+})
 
 const Rating = sequelize.define('rating', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-    rate: {type: DataTypes.INTEGER,  allowNull: false},
-});
+    rate: {type: DataTypes.INTEGER, allowNull: false},
+})
 
 const DeviceInfo = sequelize.define('device_info', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING, allowNull: false},
-    descrition: {type: DataTypes.STRING, allowNull: false},
-});
+    description: {type: DataTypes.STRING, allowNull: false},
+})
 
-const TypeBrand = sequelize.define('type_brand', { id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}, });
+const TypeBrand = sequelize.define('type_brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
 
-User.hasOne(Basket);
-Basket.belongsTo(User);
 
-User.hasMany(Rating);
-Rating.belongsTo(User);
+User.hasOne(Basket)
+Basket.belongsTo(User)
 
-Basket.hasMany(BasketDevice);
-BasketDevice.belongsTo(Basket);
+User.hasMany(Rating)
+Rating.belongsTo(User)
 
-Type.hasMany(Device);
-Device.belongsTo(Type);
+Basket.hasMany(BasketDevice)
+BasketDevice.belongsTo(Basket)
 
-Brand.hasMany(Device);
-Device.belongsTo(Brand);
+Type.hasMany(Device)
+Device.belongsTo(Type)
 
-Device.hasMany(Rating);
-Rating.belongsTo(Device);
+Brand.hasMany(Device)
+Device.belongsTo(Brand)
 
-Device.hasMany(BasketDevice);
-BasketDevice.belongsTo(Device);
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
 
-Device.hasMany(DeviceInfo);
-DeviceInfo.belongsTo(Device);
+Device.hasMany(BasketDevice)
+BasketDevice.belongsTo(Device)
 
-Type.belongsToMany(Brand, { through: TypeBrand });
-Brand.belongsToMany(Type, { through: TypeBrand });
+Device.hasMany(DeviceInfo, {as: 'info'});
+DeviceInfo.belongsTo(Device)
+
+Type.belongsToMany(Brand, {through: TypeBrand })
+Brand.belongsToMany(Type, {through: TypeBrand })
 
 module.exports = {
     User,
     Basket,
     BasketDevice,
     Device,
-    DeviceInfo,
-    Brand,
     Type,
+    Brand,
+    Rating,
     TypeBrand,
-    Rating
-};
+    DeviceInfo
+}
