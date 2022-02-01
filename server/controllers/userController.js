7d15e924b9d4122cf124b370b7302adab0cce770
Vladimir Sirenko <81309430+V-Sirenko@users.noreply.github.com>
2022-02-01 10:27:23 +0200

    Added registration logic.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,31 @@
 const ApiError = require("../error/ApiError");
+const bcrypt = require('bcrypt');
+const { User, Basket } = require('../models/models');
+const jwt = require('jsonwebtoken');
 
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
 class UserController {
-    async registration(req,res) {
-
-    };
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некорректный email или password'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
+    }
 
     async login(req,res) {
         
