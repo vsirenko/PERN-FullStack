e6687292df36ca6186d972fc5d53553631aa88cc
Vladimir Sirenko <81309430+V-Sirenko@users.noreply.github.com>
2022-02-01 16:53:37 +0200

    Init react-app, axios, react-router-dom, mobx, mobx-react-lite.

--- server/routes/deviceRouter.js
+++ server/routes/deviceRouter.js
@@ -3,7 +3,7 @@ const deviceController = require('../controllers/deviceController');
 const router = new Router();
 const  checkRole = require('../middleware/checkRoleMiddleware');
 
-router.post('/',checkRole('ADMIN'), deviceController.create);
+router.post('/', checkRole('ADMIN'), deviceController.create);
 router.get('/', deviceController.getAll);
 router.get('/:id', deviceController.getOne);
 
