a7af017f8f06794598aa85ccf44008acb8a084b5
Vladimir Sirenko <81309430+V-Sirenko@users.noreply.github.com>
2022-02-04 14:44:53 +0200

    Allmost finished device page.

--- client/src/pages/Device.js
+++ client/src/pages/Device.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 
 function Device() {
-  return <div>Device</div>;
+  const game =  {id: 1, name: 'TOM CLANCYS RAINBOW SIX SIEGE', price: 2500, rating: 5, img: `https://static.gabestore.ru/product/0_j1Ci2n2Vnd2zVuX4WATB4juk1xtRzq.jpg`}
+  return (
+  <Container className='d-flex mt-4'>
+      <Col md={4}>
+          <Image width={370} height={460} src={game.img}/>
+      </Col>
+      <Col md={4}>
+        <Row>
+        <h2>{game.name}</h2>
+        </Row>
+        <h4>Цена: {game.price} руб.</h4>
+      </Col>
+      <Col md={4} style={{marginLeft: '20px'}}>
+        <Button>Добавить в корзину</Button>
+      </Col>
+
+  </Container>
+  );
 }
 
 export default Device;
